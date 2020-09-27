d7b87abc672d1008ebe4458e94d17991c88731fd
iregueiro <iregueiro@paradigmadigital.com>
2020-09-27 20:27:01 +0200

    role for configure gpt.java

---
+++ Gpt-template.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2014-2015 José Luis Risco Martín <jlrisco@ucm.es> and 
+ * Saurabh Mittal <smittal@duniptech.com>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * http://www.gnu.org/licenses/
+ *
+ * Contributors:
+ *  - José Luis Risco Martín
+ */
+package xdevs.core.examples.efp;
+
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+import xdevs.core.modeling.Coupled;
+import xdevs.core.simulation.Coordinator;
+import xdevs.core.util.DevsLogger;
+
+/**
+ *
+ * @author jlrisco
+ */
+public class Gpt extends Coupled {
+
+    public Gpt(String name, double period, double observationTime) {
+    	super(name);
+        
+		String userDirectory = Paths.get("")
+		        .toAbsolutePath()
+		        .toString();
+		
+# ANSIBLE FICHEROS
+        
+        
+# ANSIBLE NVIRTUAL
+    	
+
+# ANSIBLE FOGSERVER
+        
+
+# ANSIBLE DATACENTER
+        
+
+# ANSIBLE CONECTOR
+        
+        
+    }
+
+    public static void main(String args[]) {
+    	
+        DevsLogger.setup(Level.INFO);
+        Gpt gpt = new Gpt("gpt", 1, 200);
+        //CoordinatorParallel coordinator = new CoordinatorParallel(gpt);
+        Coordinator coordinator = new Coordinator(gpt);
+        //RTCentralCoordinator coordinator = new RTCentralCoordinator(gpt);
+        coordinator.initialize();
+        coordinator.simulate(Long.MAX_VALUE);
+
+    }
+
+}
